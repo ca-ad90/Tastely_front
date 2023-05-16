37726caabe024786936d4aac8b1e593b772b8776
Andrea Högberg <andreahogberg@live.se>
2023-05-16 11:43:19 +0200

    test

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -8,7 +8,7 @@ const HamburgerMenu = () => {
   useEffect(() => {
     setHamburgerMenu(myHamburgerMenu());
   }, []);
-
+  let x = "2hej";
   const onToggle = () => {
     togglerIcon === "toggler__icon" ? setTogglerIcon("toggler__icon toggle") : setTogglerIcon("toggler__icon");
     collapse === "hamburger__menu" ? setCollapse("hamburger__menu nav__collapse") : setCollapse("hamburger__menu");
