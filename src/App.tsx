03281f78d95ffbede43673adf15305c0c9f4a57c
Andrea Högberg <andreahogberg@live.se>
2023-05-16 11:19:33 +0200

    Created HamburgerMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import RecipeCard from './components/RecipeCard'
-import MainApp from './components/MainApp'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import HamburgerMenu from "./components/HamburgerMenu";
+import RecipeCard from "./components/RecipeCard";
+import MainApp from "./components/MainApp";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -20,18 +21,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+
+const HamburgerMenu = () => {
+  const [collapse, setCollapse] = useState("nav__menu");
+  const [HamburgerMenu, setHamburgerMenu] = useState([]);
+  const [togglerIcon, setTogglerIcon] = useState("toggler__icon");
+
+  useEffect(() => {
+    setHamburgerMenu(myHamburgerMenu());
+  }, []);
+
+  const onToggle = () => {
+    togglerIcon === "toggler__icon" ? setTogglerIcon("toggler__icon toggle") : setTogglerIcon("toggler__icon");
+    collapse === "hamburger__menu" ? setCollapse("hamburger__menu nav__collapse") : setCollapse("hamburger__menu");
+  };
+};
+
+export default HamburgerMenu;
